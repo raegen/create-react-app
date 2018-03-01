505d11d07d1fba82ba2f0c5bdb825665281bb7bb
Nikola Pavlovic <npavlovic101@gmail.com>
2018-03-02 00:19:16 +0100

    add exif-size-loader to webpack.config

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -169,7 +169,10 @@ module.exports = {
           // A missing `test` is equivalent to a match.
           {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-            loader: require.resolve('url-loader'),
+            use: [
+              require.resolve('exif-size-loader'),
+              require.resolve('url-loader'),
+            ],
             options: {
               limit: 10000,
               name: 'static/media/[name].[hash:8].[ext]',
