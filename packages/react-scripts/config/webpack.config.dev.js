113be20de28ae20e91da4aeb016a9e30b7fc6375
Nikola Pavlovic <npavlovic101@gmail.com>
2018-03-04 18:00:03 +0100

    add react-hot-loader

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -173,6 +173,7 @@ module.exports = {
               // @remove-on-eject-begin
               babelrc: false,
               presets: [require.resolve('babel-preset-react-app')],
+              plugins: ['react-hot-loader/babel'],
               // @remove-on-eject-end
               // This is a feature of `babel-loader` for webpack (not Babel itself).
               // It enables caching results in ./node_modules/.cache/babel-loader/
